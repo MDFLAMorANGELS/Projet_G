7a3bb4d6e8100085b58a60e01ed312078d1374bc
PvalentinOC <76172386+PvalentinOC@users.noreply.github.com>
2022-02-08 09:05:55 +0100

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const helmet = require('helmet');
+const path = require('path');
+
 
 const app = express();
 
@@ -13,6 +15,9 @@ app.use((req, res, next) => {
 });
 
 app.use(express.json());
+
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
 
 app.use("/user", require("./routes/userRoutes"));
 app.use("/post", require("./routes/postRoutes"));
